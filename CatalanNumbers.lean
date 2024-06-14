b530e6e65a47aefed83a9fa45546a2fe80c27fe9
GregorSraj <143709168+GregorSraj@users.noreply.github.com>
2024-06-14 16:49:13 +0200

    fixed imports

--- CatalanNumbers.lean
+++ CatalanNumbers.lean
@@ -1,3 +1,6 @@
 -- This module serves as the root of the `CatalanNumbers` library.
 -- Import modules here that should be built as part of the library.
-import «CatalanNumbers».catalan_numbers
+import «CatalanNumbers».definitions
+import «CatalanNumbers».list_plane_tree_isomorphism
+import «CatalanNumbers».rotating_isomorphism
+import «CatalanNumbers».choose_divisibility
